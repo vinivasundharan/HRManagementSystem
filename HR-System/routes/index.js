0dc2d053c62dadcb7cf4c388f8ff409a80724941
Your Name <you@example.com>
2018-09-24 16:10:51 +0200

    m

---
+++ HR-System/routes/index.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+/* GET Employee Management page. */
+router.get('/employeeManagement', function (req, res, next) {
+  res.render('employeeManagement');
+});
+
+router.get('/searchEmployee', function (req, res, next) {
+  res.render('employee/searchEmployee');
+});
+
+router.get('/employeeResult', function (req, res, next) {
+  res.render('employee/result');
+});
+
+router.get('/vacation', function (req, res, next) {
+  res.render('employee/vacation');
+});
+
+
+
+/* GET Communication  page. */
+router.get('/communication', function (req, res, next) {
+  res.render('communication');
+});
+
+
+
+
+
+/* GET Reports page. */
+router.get('/reports', function (req, res, next) {
+  res.render('reports');
+});
+
+/* GET Survey page. */
+router.get('/surveys', function (req, res, next) {
+  res.render('surveys');
+});
+
+/* GET kpi page. */
+router.get('/kpis', function (req, res, next) {
+  res.render('kpi');
+});
+
+/* GET Workforce page. */
+router.get('/workforce', function (req, res, next) {
+  res.render('workforce');
+});
+
+
+
+
+/**
+ * Login and Logout routes and functions
+ */
+
+/* GET login page. */
+router.get('/login', function (req, res, next) {
+  res.render('login', {
+    layout: false,
+    success : global.success,
+  });
+});
+
+/* GET logout page. */
+router.get('/logout', function (req, res, next) {
+  global.login = false;
+  res.redirect('/login');
+});
+
+
+/* POST login page. */
+router.post('/login', function (req, res, next) {
+  console.log("The Body is", req.body)
+  if (req.body.email == "alex.coleman@cnbp.com" && req.body.password == "123456") {
+    global.login = true;
+    return res.redirect('/');
+  } else {
+    global.success = false;
+    res.redirect('/login');
+  }
+
+});
+
+
+module.exports = router;
